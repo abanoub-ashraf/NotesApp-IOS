b12d8d316fe5bf4fa6dbab052355d40f6f7500be
Abanoub <mrbob539@gmail.com>
2021-01-23 15:35:14 +0200

    prepare the app

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK:- LifeCycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
