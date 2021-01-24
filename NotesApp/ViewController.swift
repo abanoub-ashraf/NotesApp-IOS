48eb04bbf4b329a6e19fb46d47d6956ff12af5ae
Abanoub <mrbob539@gmail.com>
2021-01-24 14:34:59 +0200

    setup navigation controller

--- NotesApp/ViewController.swift
+++ NotesApp/ViewController.swift
@@ -1,11 +1,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK:- IBOutlets
+    @IBOutlet weak var notesTableView: UITableView!
+    
     // MARK:- LifeCycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .brown
     }
 }
